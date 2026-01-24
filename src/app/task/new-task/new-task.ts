0c3092c43c45fe3d892e483d663198a3b6625e78
Stevens Aliaga <stivensaliaga@gmail.com>
2026-01-24 11:23:30 -0500

    adding a new task component

--- src/app/task/new-task/new-task.ts
+++ src/app/task/new-task/new-task.ts
@@ -1,16 +1,30 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Task } from '../detailed-task/task.model';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task.model';
+
+
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './new-task.html',
   styleUrl: './new-task.css',
 })
 export class NewTask {
   @Output() cancel = new EventEmitter<void>()
-
+  @Output() addTask = new EventEmitter<NewTaskData>()
+  enteredTitle = "";
+  enteredSummary = "";
+  enteredDate = "";
   onCancel() {
     this,this.cancel.emit();
+  }
+  onAddTask(){
+    this.addTask.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate
+    });
   }
 }
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { DetailedTask } from "./detailed-task/detailed-task";
 import { NewTask } from "./new-task/new-task";
+import { NewTaskData } from "./task.model";
 
 @Component({
     selector: "app-task",
@@ -10,14 +11,27 @@ import { NewTask } from "./new-task/new-task";
     styleUrl: "./task.component.css"
 })
 export class TaskComponent {
-    @Input({ required: true }) userId?: string;
-    @Input({ required: true }) name?: string;
+    @Input({ required: true }) userId!: string;
+    @Input({ required: true }) name!: string;
     addingTask!: boolean;
     onAddTask () {
         this.addingTask =  true; 
     }
     onCancel () {
         this.addingTask =  false; 
+    }
+    addTask(newData: NewTaskData) {
+        const taskId = this.detailed_tasks[this.detailed_tasks.length - 1].id;
+        this.detailed_tasks.push({
+            id: "t" +  ( Number(taskId.split("t")[1]) + 1 ),
+            userId: this.userId,
+            title: newData.title,
+            summary: newData.summary,
+            dueDate: newData.date
+        });
+        this.addingTask = false;
+        console.log(this.detailed_tasks);
+        
     }
 
     detailed_tasks = [
