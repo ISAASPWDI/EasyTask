cb91b57659c67bdbefc3f6c152f6881021568c41
Stevens Aliaga <stivensaliaga@gmail.com>
2026-01-24 11:41:14 -0500

    creating a new component for sharing styles in multiple components and adding some pipes for data format

--- src/app/task/detailed-task/detailed-task.ts
+++ src/app/task/detailed-task/detailed-task.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../task.model';
-
+import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common'
 
 
 @Component({
   selector: 'app-detailed-task',
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './detailed-task.html',
   styleUrl: './detailed-task.css',
 })
